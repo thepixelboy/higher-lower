41f6614efe164bae45e42f46578993c49b09584c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-07-30 20:10:13 +0200

    feat: final code

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask
+import random
+
+random_number = random.randint(0, 9)
+print(random_number)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return (
+        "<h1 style='font-family:Arial, Helvetica, sans-serif'>Guess a number between 0 and 9</h1>"
+        "<img src='https://media.giphy.com/media/ne3xrYlWtQFtC/giphy.gif'/>"
+    )
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return (
+            "<h1 style='font-family:Arial, Helvetica, sans-serif; color: red'>Too high, try again!</h1>"
+            "<img src='https://media.giphy.com/media/4EESDe2IckKldhxFsu/giphy.gif'/>"
+        )
+
+    elif guess < random_number:
+        return (
+            "<h1 style='font-family:Arial, Helvetica, sans-serif; color: red'>Too low, try again!</h1>"
+            "<img src='https://media.giphy.com/media/S5haphg9c4ZTiaLemY/giphy.gif'/>"
+        )
+    else:
+        return (
+            "<h1 style='font-family:Arial, Helvetica, sans-serif; color: green'>You found me!</h1>"
+            "<img src='https://media.giphy.com/media/xT5LMGx4ZvjJMjXOxy/giphy.gif'/>"
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
